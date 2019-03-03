3405d38a13322060dd6a1c8fde3dbe652ebf8127
maoznir <nir.maoz@cloudinary.com>
2019-03-03 04:10:59 +0200

    added WebRTC script file, updated MainPage design

--- src/Components/InvitePage/InvitePage.js
+++ src/Components/InvitePage/InvitePage.js
@@ -1,118 +1,126 @@
 import React from 'react';
 import { Page } from '../Components';
-import { Link } from "react-router-dom";
-import initLS from 'cloudinary-live-stream'
-//import adapter from 'webrtc-adapter';
-
+import { Link } from 'react-router-dom';
+import initLS from 'cloudinary-live-stream';
 
 import {
-    Container, Col, Row, Form,
-    FormGroup, Label, Input,
-    Button,
+  Container,
+  Col,
+  Row,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  Button
 } from 'reactstrap';
 
-import './InvitePage.css'
+import './InvitePage.css';
 
-const CLD_API_HOST = "api.cloudinary.com";
-const CLD_RES_HOST = "res.cloudinary.com";
-const CLD_WEB_RTC_HOST = "webrtc-api.cloudinary.com";
-const CLOUD_NAME = "demo-live";
-const UPLOAD_PRESET = "live-stream";
-const UPLOAD_PRESET_OPENER = "live-opener";
+const CLD_API_HOST = 'api.cloudinary.com';
+const CLD_RES_HOST = 'res.cloudinary.com';
+const CLD_WEB_RTC_HOST = 'webrtc-api.cloudinary.com';
+const CLOUD_NAME = 'demo-live';
+const UPLOAD_PRESET = 'live-stream';
+const UPLOAD_PRESET_OPENER = 'live-opener';
 const CLOUD_NAME_IMAGES = CLOUD_NAME;
-const UPLOAD_PRESET_IMAGES = "images";
-const UPLOAD_WIDGET_PREFIX = "https://widget.cloudinary.com";
-const UPLOAD_TYPE = "upload";
+const UPLOAD_PRESET_IMAGES = 'images';
+const UPLOAD_WIDGET_PREFIX = 'https://widget.cloudinary.com';
+const UPLOAD_TYPE = 'upload';
 
 export default class InvitePage extends React.PureComponent {
-    constructor(props) {
-        super(props);
-        this.state = {
-            cloudName: CLOUD_NAME,
-            uploadPreset: UPLOAD_PRESET
+  constructor(props) {
+    super(props);
+    this.state = {
+      cloudName: CLOUD_NAME,
+      uploadPreset: UPLOAD_PRESET
+    };
+    this.liveStream = {};
+    this.videoRef = React.createRef();
+    this.startLiveStream = this.startLiveStream.bind(this);
+    this.stop = this.stop.bind(this);
+  }
+
+  componentWillUnmount() {
+    //destroy livestream
+  }
+
+  componentDidMount() {
+    this.initLiveStream({ ...this.state });
+  }
+
+  startLiveStream(liveStream, publicId, url) {
+    this.liveStream = liveStream;
+    this.liveStream.start(publicId);
+    this.setState(state => state);
+  }
+
+  stop() {
+    this.liveStream.stop();
+  }
+
+  // call initLiveStream with the configuration parameters:
+  initLiveStream({ cloudName, uploadPreset }) {
+    let startLiveStream = this.startLiveStream;
+    let liveStream;
+    let video = this.videoRef.current;
+    console.log('initlivestream, video ref:', video);
+    initLS({
+      cloudName: cloudName,
+      uploadPreset: uploadPreset,
+      debug: 'all',
+      hlsTarget: true,
+      fileTarget: true,
+      events: {
+        start: function(args) {
+          // user code
+          console.log('JANUS START !!! args:', args);
+        },
+        stop: function(args) {
+          // user code
+          console.log('JANUS STOP !!!');
+        },
+        error: function(error) {
+          // user code
+          console.log('JANUS ERROR !!!:', error);
+        },
+        local_stream: function(stream) {
+          // user code, typically attaching the stream to a video view:
+          console.log('JASNUS LOCAL_STREAM !!!, stream:', stream);
+          liveStream.attach(video, stream);
         }
-        this.liveStream = {};
-        this.videoRef = React.createRef();
-        this.startLiveStream = this.startLiveStream.bind(this);
-        this.stop = this.stop.bind(this);
-    }
+      }
+    }).then(result => {
+      // keep handle to instance to start/stop streaming
+      liveStream = result;
 
-    componentWillUnmount() {
-        //destroy livestream
-    }
+      // Extract public id and url from result (publish the url for people to watch the stream):
+      let publicId = result.response.public_id;
+      let url = result.response.secure_url;
 
-    componentDidMount() {
-        this.initLiveStream({...this.state});
-    }
+      // start the streaming:
+      startLiveStream(liveStream, publicId, url);
+    });
+  }
 
-    startLiveStream(liveStream, publicId, url){
-        this.liveStream = liveStream;
-        this.liveStream.start(publicId);
-        this.setState(state=>state);
-    }
-
-    stop(){
-        this.liveStream.stop();        
-    }
-
-    // call initLiveStream with the configuration parameters:
-    initLiveStream({cloudName, uploadPreset}) {
-        let startLiveStream = this.startLiveStream;
-        let liveStream;
-        let video = this.videoRef.current;
-        console.log("initlivestream, video ref:",video);
-        initLS({
-            cloudName: cloudName,
-            uploadPreset: uploadPreset,
-            debug: "all",
-            hlsTarget: true,
-            fileTarget: true,
-            events: {
-                start: function (args) {
-                    // user code
-                    console.log('JANUS START !!! args:',args);
-                },
-                stop: function (args) {
-                    // user code
-                    console.log('JANUS STOP !!!');
-                },
-                error: function (error) {
-                    // user code
-                    console.log('JANUS ERROR !!!:', error);
-                },
-                local_stream: function (stream) {
-                    // user code, typically attaching the stream to a video view:
-                    console.log("JASNUS LOCAL_STREAM !!!, stream:", stream);
-                    liveStream.attach(video, stream);
-                }
-            }
-        }).then((result) => {
-            // keep handle to instance to start/stop streaming 
-            liveStream = result;
-
-
-            // Extract public id and url from result (publish the url for people to watch the stream):
-            let publicId = result.response.public_id;
-            let url = result.response.secure_url;
-
-            // start the streaming:
-            startLiveStream(liveStream, publicId, url);
-        })
-    }
-
-    render() {
-        const video = this.videoRef;
-        console.log('video ref:',video);
-        return (
-            <Page>
-                <h1 className="whitecolor">Invite Page</h1>
-                <p>{this.props.values.social}</p>
-                <Link to="/">Home</Link>
-                <div className="video-wrapper">
-                    <video ref={video} className="video" id="video" autoPlay muted="muted" playsInline />
-                </div>
-                <button onClick={() => this.stop()}>stop</button>
-            </Page>
-        );
-    }
+  render() {
+    const video = this.videoRef;
+    console.log('video ref:', video);
+    return (
+      <Page>
+        <h1 className="whitecolor">Invite Page</h1>
+        <Link to="/">Home</Link>
+        <div className="video-wrapper">
+          <video
+            ref={video}
+            className="video"
+            id="video"
+            autoPlay
+            muted="muted"
+            playsInline
+          />
+        </div>
+        <button onClick={() => this.stop()}>stop</button>
+      </Page>
+    );
+  }
 }
--- src/Components/MainPage/MainPage.js
+++ src/Components/MainPage/MainPage.js
@@ -1,171 +1,200 @@
 import React from 'react';
 import { Page } from '../Components';
-import { Link } from "react-router-dom";
-import { Field, Form, Formik } from "formik";
-import { Col, Container, Row, Button} from "reactstrap";
-import { ReactstrapInput, ReactstrapRadio } from "reactstrap-formik";
+import { Link } from 'react-router-dom';
+import { Field, Form, Formik } from 'formik';
+import { Col, Container, Row, Button } from 'reactstrap';
+import { ReactstrapInput, ReactstrapRadio } from 'reactstrap-formik';
+import CloudUpload from '@material-ui/icons/CloudUpload';
+import Slideshow from '@material-ui/icons/Slideshow';
+import Vignette from '@material-ui/icons/Vignette';
+import BlurOn from '@material-ui/icons/BlurOn';
 
-import './MainPage.css'
 
-const ButtonToNavigate = ({ history, values, to, type, children, doBefore }) => {
-    return (
-    <Button color="primary" className="arrow"
+import './MainPage.css';
+
+const facebookLabel = () =>{
+  return (
+    <label htmlFor="facebook">
+    <div className="icon facebook-icon"></div>
+    <span>Facebook</span>
+    </label>
+  );
+}
+
+const youtubeLabel = () =>{
+  return (
+    <label htmlFor="youtube">
+    <div className="icon youtube-icon"></div>
+    <span>Youtube</span>
+    </label>
+  );
+}
+
+const ButtonToNavigate = ({
+  history,
+  values,
+  to,
+  type,
+  children,
+  doBefore
+}) => {
+  return (
+    <Button
+      color="primary"
+      className="arrow"
       type={type}
       onClick={() => {
-          if (doBefore){
-              doBefore(values);
-          }
-          history.push(to)}
+        if (doBefore) {
+          doBefore(values);
         }
+        history.push(to);
+      }}
     >
       {children}
     </Button>
   );
-    }
+};
 
 export default class MainPage extends React.PureComponent {
-    constructor(props) {
-        super(props);
-        this.state = {
-            title: 'My live video',
-            effects: {
-                logo: null,
-                intro: null,
-                border: null,
-                blur: null
-            },
-            social: false
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: 'My live video',
+      effects: {
+        logo: null,
+        intro: null,
+        border: null,
+        blur: null
+      },
+      social: false
+    };
+  }
 
-    onNext(){
-        this.props.updateInvite(this.state);
-    }
+  onNext() {
+    this.props.updateInvite(this.state);
+  }
 
-    render() {
-        const doBefore = this.props.updateInvite;
-        return (
-            <Page>
-                
-                <Formik
-      initialValues={{ title: "My live video", social: "none" }}
-      validate={values => {
-        const errors = {};
-        if (!values.title) {
-          errors.email = "Required";
-        } else if (
-          !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
-        ) {
-          errors.email = "Invalid email address";
-        }
-        return errors;
-      }}
-      onSubmit={(values, { setSubmitting }) => {
-        //console.log(values);
-        //Make API calls here
+  render() {
+    const doBefore = this.props.updateInvite;
+    return (
+      <Page>
+        <Formik
+          initialValues={{ title: 'My live video', social: 'none' }}
+          validate={values => {
+            const errors = {};
+            if (!values.title) {
+              errors.email = 'Required';
+            } else if (
+              !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
+            ) {
+              errors.email = 'Invalid email address';
+            }
+            return errors;
+          }}
+          onSubmit={(values, { setSubmitting }) => {
+            //console.log(values);
+            //Make API calls here
 
-        setTimeout(() => {
-          setSubmitting(false);
-          alert(
-            `Submitted Successfully ->  ${JSON.stringify(values, null, 2)}`
-          );
-        }, 2000);
-      }}
-      render={({ submitForm, isSubmitting, values }) => (
-        <Form>
-          <Container style={{ paddingTop: "5px" }} className="text-white">
-            <Row>
-              <Col xs="12">
-                <Field
-                  label="Live streaming title"
-                  placeholder="My live video"
-                  name="title"
-                  id="title"                  
-                  component={ReactstrapInput}
-                />
-              </Col>
-              <Col xs="12">
-              <h3>Effects</h3>
-              </Col>
-              <Col xs="12">
-              <input
-            type="checkbox"
-            name="logo"
-            value="logo"
-          />
-          <span style={{marginLeft:"5px"}}>Add your logo</span>
-          </Col>
-          <Col xs="12">
-          <input
-            type="checkbox"
-            name="intro"
-            value="intro"
-          />
-          <span style={{marginLeft:"5px"}}>Add intro animation</span>
-          </Col>
-          <Col xs="12">
-          <input
-            type="checkbox"
-            name="border"
-            value="border"
-          />
-          <span style={{marginLeft:"5px"}}>Apply vignette border</span>
-          </Col>
-          <Col xs="12">
-          <input
-            type="checkbox"
-            name="blur"
-            value="blur"
-          />
-          <span style={{marginLeft:"5px"}}>Blur your video</span>
-          <hr/>
-          </Col>
-          <Col xs="12">
-              <h3>Stream to social media</h3>
-              </Col>
-          
-          <Col xs="12">
-          <Field
-                    name="social"
-                    component={ReactstrapRadio}
-                    value="none"
-                    type="radio"
-                    label="None"
-                    checked={true}
-                />
-          </Col>
-          <Col xs="12">
-          <Field
-                    name="social"
-                    component={ReactstrapRadio}
-                    value="facebook"
-                    type="radio"
-                    label="Facebook"
-                />
-          </Col>
-          <Col xs="12">
-          <Field
-                    name="social"
-                    component={ReactstrapRadio}
-                    value="youtube"
-                    type="radio"
-                    label="Youtube"
-                />
-                <hr/>
-          </Col>
-              <Col xs="12">
-              <ButtonToNavigate doBefore={doBefore} values={values} to="/invite" type="submit" history={this.props.history}>→</ButtonToNavigate>
-              </Col>
-            </Row>
-            <pre>{JSON.stringify(values, null, 2)}</pre>
-          </Container>
-        </Form>
-              )}
-              />
+            setTimeout(() => {
+              setSubmitting(false);
+              alert(
+                `Submitted Successfully ->  ${JSON.stringify(values, null, 2)}`
+              );
+            }, 2000);
+          }}
+          render={({ submitForm, isSubmitting, values }) => (
+            <Form>
+              <Container style={{ paddingTop: '5px' }} className="text-white">
+                <Row>
+                  <Col xs="12">
+                    <Field
+                      label="Live streaming title"
+                      placeholder="My live video"
+                      name="title"
+                      id="title"
+                      component={ReactstrapInput}
+                    />
+                  </Col>
+                  <Col xs="12">
+                    <h4>Effects</h4>
+                  </Col>
+                  <Col xs="12">
+                    <input type="checkbox" name="logo" value="logo"/>
+                    <span style={{ marginLeft: '5px' }}><CloudUpload className="svg-icons"/>Add your logo</span>
+                  </Col>
+                  <Col xs="12">
+                    <input type="checkbox" name="intro" value="intro" />
+                    <span style={{ marginLeft: '5px' }}>
+                    <Slideshow className="svg-icons"/>
+                      Add intro animation
+                    </span>
+                  </Col>
+                  <Col xs="12">
+                    <input type="checkbox" name="border" value="border" />
+                    <span style={{ marginLeft: '5px' }}>
+                    <Vignette className="svg-icons"/>
+                      Apply vignette border
+                    </span>
+                  </Col>
+                  <Col xs="12">
+                    <input type="checkbox" name="blur" value="blur" />
+                    <span style={{ marginLeft: '5px' }}>
+                    <BlurOn className="svg-icons"/>
+                    Blur your video</span>
+                    <hr />
+                  </Col>
+                  <Col xs="12">
+                    <h4>Stream to social media</h4>
+                  </Col>
 
-
-                <Link to="/invite">Invite</Link>
-            </Page>
-        );
-    }
+                  <Col xs="12">
+                    <Field
+                      name="social"
+                      component={ReactstrapRadio}
+                      value="none"
+                      type="radio"
+                      label="None"
+                      checked={true}
+                    />
+                  </Col>
+                  <Col xs="12">
+                    <Field
+                      name="social"
+                      component={ReactstrapRadio}
+                      value="facebook"
+                      type="radio"
+                      label={facebookLabel()}
+                    >aaa</Field>
+                  </Col>
+                  <Col xs="12">
+                    <Field
+                      name="social"
+                      component={ReactstrapRadio}
+                      value="youtube"
+                      type="radio"
+                      label={youtubeLabel()}
+                    />
+                    <hr />
+                  </Col>
+                  <Col xs="12" >
+                  <Row className="justify-content-center align-items-center">
+                    <ButtonToNavigate
+                      doBefore={doBefore}
+                      values={values}
+                      to="/invite"
+                      type="submit"
+                      history={this.props.history}
+                    >
+                      →
+                    </ButtonToNavigate>
+                    </Row>
+                  </Col>
+                </Row>
+              </Container>
+            </Form>
+          )}
+        />
+      </Page>
+    );
+  }
 }
