e8bf9bd72dcb1851e4e3e4fdb213e4b34112280f
maoznir <nir.maoz@cloudinary.com>
2019-03-17 17:58:38 +0200

    Update pathname for github pages

---
+++ docs/precache-manifest.a39a102e8cda6503b52c2567f52795c7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "2691e9a9793b0913627071a894d0a067",
+    "url": "/react-video-live-demo/static/media/logo-white.2691e9a9.svg"
+  },
+  {
+    "revision": "6e732f62cb2e52e0fe62",
+    "url": "/react-video-live-demo/static/css/main.73126580.chunk.css"
+  },
+  {
+    "revision": "e12248b8006c340b5c95",
+    "url": "/react-video-live-demo/static/js/runtime~main.e12248b8.js"
+  },
+  {
+    "revision": "f9251f35b77a30dd6dbdc9e5768e656e",
+    "url": "/react-video-live-demo/static/media/logo-with-text.f9251f35.svg"
+  },
+  {
+    "revision": "c34870d4f6a01a346dcb",
+    "url": "/react-video-live-demo/static/js/2.c34870d4.chunk.js"
+  },
+  {
+    "revision": "6e732f62cb2e52e0fe62",
+    "url": "/react-video-live-demo/static/js/main.6e732f62.chunk.js"
+  },
+  {
+    "revision": "a47393c134c3e33bdbe9f3bff562da1b",
+    "url": "/react-video-live-demo/static/media/app-background-small.a47393c1.jpg"
+  },
+  {
+    "revision": "449319d38f74a82d03ea1ae0f551bcc7",
+    "url": "/react-video-live-demo/static/media/youtube.449319d3.svg"
+  },
+  {
+    "revision": "575419bd3cebf76f59109014b7d07b76",
+    "url": "/react-video-live-demo/static/media/facebook.575419bd.svg"
+  },
+  {
+    "revision": "c34870d4f6a01a346dcb",
+    "url": "/react-video-live-demo/static/css/2.d631316b.chunk.css"
+  },
+  {
+    "revision": "d9a8c91f084f5917ba05534cadfc39ce",
+    "url": "/react-video-live-demo/index.html"
+  }
+];
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/live/precache-manifest.d7516b77fa195ae7683622dd4798f661.js"
+  "/react-video-live-demo/precache-manifest.a39a102e8cda6503b52c2567f52795c7.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/live/index.html", {
+workbox.routing.registerNavigationRoute("/react-video-live-demo/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
