f129216206f3b9f3c67d0f68d4e387a70a10c955
maoznir <nir.maoz@cloudinary.com>
2019-03-17 17:38:35 +0200

    Move demo to doc folder

---
+++ docs/precache-manifest.d7516b77fa195ae7683622dd4798f661.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "2691e9a9793b0913627071a894d0a067",
+    "url": "/live/static/media/logo-white.2691e9a9.svg"
+  },
+  {
+    "revision": "9d1b2e0d3679fbcdbc04",
+    "url": "/live/static/css/main.8cd8db37.chunk.css"
+  },
+  {
+    "revision": "5281855c85e6fc35b2f2",
+    "url": "/live/static/js/runtime~main.5281855c.js"
+  },
+  {
+    "revision": "f9251f35b77a30dd6dbdc9e5768e656e",
+    "url": "/live/static/media/logo-with-text.f9251f35.svg"
+  },
+  {
+    "revision": "ab1d678a16569f1196a4",
+    "url": "/live/static/js/2.ab1d678a.chunk.js"
+  },
+  {
+    "revision": "9d1b2e0d3679fbcdbc04",
+    "url": "/live/static/js/main.9d1b2e0d.chunk.js"
+  },
+  {
+    "revision": "a47393c134c3e33bdbe9f3bff562da1b",
+    "url": "/live/static/media/app-background-small.a47393c1.jpg"
+  },
+  {
+    "revision": "575419bd3cebf76f59109014b7d07b76",
+    "url": "/live/static/media/facebook.575419bd.svg"
+  },
+  {
+    "revision": "449319d38f74a82d03ea1ae0f551bcc7",
+    "url": "/live/static/media/youtube.449319d3.svg"
+  },
+  {
+    "revision": "ab1d678a16569f1196a4",
+    "url": "/live/static/css/2.d631316b.chunk.css"
+  },
+  {
+    "revision": "e6891af2d8bee4d16199498e54399591",
+    "url": "/live/index.html"
+  }
+];
