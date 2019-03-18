f4320a69580cfb22d05d120818726f4a82efb921
maoznir <nir.maoz@cloudinary.com>
2019-03-18 15:50:07 +0200

    Add title or subject to share options

--- src/Components/Share/Share.js
+++ src/Components/Share/Share.js
@@ -13,25 +13,40 @@ import {
   WhatsappIcon
 } from 'react-share';
 
-const Share = ({ url, className="" }) => {
+const Share = ({ url, className = '' }) => {
   return (
     <Col xs={12} className={className}>
-    <Row className="justify-content-center align-items-center">
-      <EmailShareButton url={url} >
-        <EmailIcon size={32} round={true} />
-      </EmailShareButton>
-      <TwitterShareButton url={url} >
-        <TwitterIcon size={32} round={true} />
-      </TwitterShareButton>
-      <FacebookShareButton url={url} >
-        <FacebookIcon size={32} round={true} />
-      </FacebookShareButton>
-      <LinkedinShareButton url={url} >
-        <LinkedinIcon size={32} round={true} />
-      </LinkedinShareButton>
-      <WhatsappShareButton url={url} >
-        <WhatsappIcon size={32} round={true} />
-      </WhatsappShareButton>
+      <Row className="justify-content-center align-items-center">
+        <EmailShareButton
+          url={url}
+          subject="Join my live video broadcast: 'My live video' @cloudinary live streaming"
+        >
+          <EmailIcon size={32} round={true} />
+        </EmailShareButton>
+        <TwitterShareButton
+          url={url}
+          title="Join my live video broadcast: 'My live video' @cloudinary live streaming"
+        >
+          <TwitterIcon size={32} round={true} />
+        </TwitterShareButton>
+        <FacebookShareButton
+          url={url}
+          quote="Join my live video broadcast: 'My live video' @cloudinary live streaming"
+        >
+          <FacebookIcon size={32} round={true} />
+        </FacebookShareButton>
+        <LinkedinShareButton
+          url={url}
+          title="Join my live video broadcast: 'My live video' @cloudinary live streaming"
+        >
+          <LinkedinIcon size={32} round={true} />
+        </LinkedinShareButton>
+        <WhatsappShareButton
+          url={url}
+          title="Join my live video broadcast: 'My live video' @cloudinary live streaming"
+        >
+          <WhatsappIcon size={32} round={true} />
+        </WhatsappShareButton>
       </Row>
     </Col>
   );
