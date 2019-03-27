46e309e10e08d7d1adf73e9c0b721725fd10d9d2
maoznir <nir.maoz@cloudinary.com>
2019-03-27 10:19:21 +0200

    Center loading spinner

--- src/Components/Loader/Loader.js
+++ src/Components/Loader/Loader.js
@@ -5,7 +5,7 @@ import { Spinner, Container, Row, Col } from 'reactstrap';
 const Loader = ({text, absolute}) => {
   return (
       <Container className={"h-100 text-white"} style={absolute ? {position:"absolute"}: {}}>
-        <Row className="loader-container text-center" />
+        <Row className="loader-container justify-content-center align-items-center text-center" />
         <Row className="justify-content-center align-items-center">
           <Col mx={12} className="text-center">
           <Spinner className="spinner-size" color="light"/>
